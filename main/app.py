ae78bf4c38b6a3a18c8e8014cee930528ae41ba3
Pranav Pawar <104649411+pranavspawar@users.noreply.github.com>
2026-06-23 00:06:46 +0530

    Fix syntax error in grade calculation

--- main/app.py
+++ main/app.py
@@ -1,12 +1,12 @@
 #Simple python code
 #check in error
 #add ruff to check code 
-sdkjnd
+
 score = 85
 
 if score >= 90:
     print("Grade: A")
 elif score >= 80:
     print("Grade: B")
-else
-print("Grade: C")
+else:
+    print("Grade: C")
